b79262e170d1febce41c25d4141b4a697ea5e7c3
小新 <1066321456@qq.com>
2023-12-23 21:51:40 +0800

    feat: 更新蜂鸣器和烟雾报警器

--- src/mainPro.c
+++ src/mainPro.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
 #include <string.h>
 #include <errno.h>
-#include <unistd.h>
 #include "controlDevice.h"
  
 struct Devices* findDeviceByName(char *name, struct Devices *phead)
 {
-    struct Devices *tmp = phead;
-    if (phead == NULL) {
+    struct Devices *tmp =phead;
+    if(phead == NULL){
         return NULL;
-    } else {
-        while (tmp != NULL) {
-            if (strcmp(tmp->deviceName, name)==0) {
+    }else{
+        while(tmp != NULL){
+            if(strcmp(tmp->deviceName,name)==0){
                 return tmp;
             }
             tmp = tmp->next;
@@ -22,30 +21,42 @@ struct Devices* findDeviceByName(char *name, struct Devices *phead)
  
 int main()
 {
-    char name[128];
+    char *smokeName = "smokeAlarm";
+    char *buzzerName = "buzzer";
     struct Devices *tmp = NULL;
+    int smokeStatus;												//存放“烟雾传感器”状态
  
-    if (wiringPiSetup() == -1) {  // 初始化wiringPi库，如果初始化失败则打印
-        fprintf (stdout, "Unable to start wiringPi: %s\n", strerror(errno)) ; 
+    if (wiringPiSetup () == -1) { 
+        fprintf (stdout, "Unable to start wiringPi: %s\n", strerror (errno)) ; 
         return 1 ; 
     }
  
     struct Devices *pdeviceHead = NULL;				                    //定义初始链表头
-    pdeviceHead = addBathroomLightToDeviceLink(pdeviceHead);            //“浴室灯”加入设备链表
-    pdeviceHead = addBedroomLightToDeviceLink(pdeviceHead);
-    pdeviceHead = addRestaurantLightToDeviceLink(pdeviceHead);
-    pdeviceHead = addLivingroomLightToDeviceLink(pdeviceHead);
+    //pdeviceHead = addBathroomLightToDeviceLink(pdeviceHead);            //“浴室灯”加入设备链表
+    //pdeviceHead = addBedroomLightToDeviceLink(pdeviceHead);
+    //pdeviceHead = addRestaurantLightToDeviceLink(pdeviceHead);
+    //pdeviceHead = addLivingroomLightToDeviceLink(pdeviceHead);
+    pdeviceHead = addSmokeAlarmToDeviceLink(pdeviceHead);
+    pdeviceHead = addBuzzerToDeviceLink(pdeviceHead);
  
-    while (1) {
-        printf("Input:\n");
-        scanf("%s", name);
-        tmp = findDeviceByName(name, pdeviceHead); // 在设备链表中根据名称查找设备，返回设备指针
-        if (tmp != NULL) {
-            tmp->Init(tmp->pinNum);         //先初始化
-            tmp->open(tmp->pinNum);
-            sleep(4);
-            tmp->close(tmp->pinNum);
+    while(1){
+        tmp = findDeviceByName(smokeName, pdeviceHead);
+        if(tmp != NULL){
+            tmp->Init(tmp->pinNum);
+            smokeStatus = tmp->readStatus(tmp->pinNum);
+            printf("state = %d\n", smokeStatus);
+            tmp = findDeviceByName(buzzerName, pdeviceHead);
+            if(tmp != NULL){
+                if( smokeStatus == 0 ){
+                    tmp->Init(tmp->pinNum);
+                    tmp->open(tmp->pinNum);
+                }else{
+                    tmp->Init(tmp->pinNum);
+                    tmp->close(tmp->pinNum);
+                }           
+            }
         }
     }
+ 
     return 0;
 }
